abae1ac4a9b26b88dfb99ad0f82433b256cc8b39
igggys@gmail.com <igggys@gmail.com>
2023-11-27 10:30:48 +0200

    adding class Product in ModelsLib class

---
+++ ModelsLib/Products/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelsLib.Products
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public float WholesalePrice { get; set; }
+        [Range(0, 60)]
+        public uint AlcoholByVolume { get; set; } //
+        [Range(0, 120)]
+        public uint? InternationalBitternessUnits { get; set; }
+        public EuropeanBrewingConvention EuropeanBrewingConvention { get; set; }
+    }
+
+    public enum EuropeanBrewingConvention
+    {
+        PaleStraw = 1,
+        Straw = 2,
+        PaleGold = 3,
+        DeepGold = 4,
+        PaleAmber = 5,
+        MediumAmber = 6,
+        DeepAmber = 7,
+        AmberBrown = 8,
+        Brown = 9,
+        RubyBrown = 10,
+        DeepBrown = 11,
+        Black = 12
+    }
+}
