300c4ad1a50a696577846ec831435f9938605c12
igggys@gmail.com <igggys@gmail.com>
2023-11-27 10:36:47 +0200

    changes in the class Product from ModelsLib

--- ModelsLib/Products/Product.cs
+++ ModelsLib/Products/Product.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace ModelsLib.Products
 {
     public class Product
     {
         public Guid ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string Description { get; set; }
+        public ProductText[] ProducTexts { get; set; }
         public float WholesalePrice { get; set; }
         [Range(0, 60)]
         public uint AlcoholByVolume { get; set; } //
@@ -34,5 +34,12 @@ namespace ModelsLib.Products
         RubyBrown = 10,
         DeepBrown = 11,
         Black = 12
+    }
+
+    public class ProductText
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CultureInfo Language { get; set; }
     }
 }
