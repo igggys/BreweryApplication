e479c327a325aae867040683c892eb9c2a8787fa
igggys@gmail.com <igggys@gmail.com>
2023-12-19 08:33:14 +0200

    Changes design of the public site

---
+++ BreweryApplication/wwwroot/js/Masters/MasterRegistrations.js
@@ -0,0 +1,24 @@
+﻿let ThisPage = new function () {
+
+    this.LanguagesPopupMenuVisible;
+    this.LanguagesMenuShow = function () {
+        let popUpMenu = document.getElementById("LanguagesPopupMenu");
+
+        if (this.LanguagesPopupMenuVisible) {
+            popUpMenu.style.display = "none";
+            this.LanguagesPopupMenuVisible = false;
+            return;
+        }
+
+        popUpMenu.style.display = "flex";
+        this.LanguagesPopupMenuVisible = true;
+        event.stopPropagation();
+    }
+}
+
+ThisPage.LanguagesPopupMenuVisible = false;
+window.onclick = function (event) {
+    if (ThisPage.LanguagesPopupMenuVisible) {
+        ThisPage.LanguagesMenuShow();
+    }
+}
